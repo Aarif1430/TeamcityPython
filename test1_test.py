49355b48c31dc14c0b4ea5121622733926012c5f
Aarif1430 <malikarif13@gmail.com>
2019-08-18 13:43:50 +0100

    add

--- test1_test.py
+++ test1_test.py
@@ -1,5 +1,7 @@
 import unittest
 from test1 import sum, multi, minus
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
 
 class TestSum(unittest.TestCase):
 
@@ -13,4 +15,8 @@ class TestSum(unittest.TestCase):
         self.assertEqual(multi(2,3), 6, "Should be 6")
 
 if __name__ == '__main__':
-    unittest.main()
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
